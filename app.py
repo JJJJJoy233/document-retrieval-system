0d77e87e3c0cc376c38c195167a11b98eda980c9
JJJJJoy233 <403239273@qq.com>
2023-06-29 09:06:39 +0800

    add main logic to server and modify the logic of the system

---
+++ app.py
@@ -0,0 +1,151 @@
+import os
+import re
+import sqlite3
+from docx import Document
+from PyPDF2 import PdfFileReader
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# 创建SQLite数据库
+def create_database():
+    conn = sqlite3.connect('index.db')
+    c = conn.cursor()
+
+    # 创建索引表
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS file_index (
+            file_path TEXT,
+            line_number INTEGER,
+            content TEXT
+        )
+    ''')
+    c.execute('''
+              DELETE FROM file_index
+              ''')
+    conn.commit()
+    conn.close()
+
+
+# 将指定文件夹下的PDF和Word文档建立索引并存储到数据库
+def build_index(folder_path):
+    conn = sqlite3.connect('index.db')
+    c = conn.cursor()
+
+    # 遍历文件夹中的所有文件
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+
+            # 处理PDF文件
+            if file.endswith('.pdf'):
+                pdf = PdfFileReader(open(file_path, 'rb'))
+                total_pages = pdf.getNumPages()
+
+                for page_number in range(total_pages):
+                    page = pdf.getPage(page_number)
+                    content = page.extract_text()
+                    lines = content.split('\n')
+
+                    # 遍历每一行文本
+                    for line_number, line in enumerate(lines):
+                        # 忽略空行和只包含空格的行
+                        if line.strip():
+                            # 插入索引到数据库
+                            c.execute('INSERT INTO file_index VALUES (?, ?, ?)', (file_path, line_number, line))
+
+            # 处理Word文档
+            elif file.endswith('.docx'):
+                doc = Document(file_path)
+
+                # 遍历每个段落
+                for paragraph_number, paragraph in enumerate(doc.paragraphs):
+                    content = paragraph.text.strip()
+
+                    # 忽略空段落
+                    if content:
+                        # 插入索引到数据库
+                        c.execute('INSERT INTO file_index VALUES (?, ?, ?)', (file_path, paragraph_number, content))
+
+    conn.commit()
+    conn.close()
+
+
+# 根据关键字搜索索引并返回匹配的结果
+def search_index(keyword):
+    conn = sqlite3.connect('index.db')
+    c = conn.cursor()
+
+    # 使用SQL查询匹配的索引
+    c.execute('SELECT file_path, line_number, content FROM file_index WHERE content LIKE ?', ('%' + keyword + '%',))
+    results = c.fetchall()
+
+    # 构建结果列表
+    search_results = []
+    current_file = None
+    current_lines = []
+    
+    for result in results:
+        file_path, line_number, content = result
+        # search_results.append({file_path: {line_number: content}})
+        if current_file != file_path:
+            if current_file:
+                search_results.append({'file_path': current_file, 'lines': current_lines})
+            current_file = file_path
+            current_lines = [{'line_number': line_number, 'line_content': content}]
+        else:
+            current_lines.append({'line_number': line_number, 'line_content': content})
+
+    if current_file:
+        search_results.append({'file_path': current_file, 'lines': current_lines})
+
+    conn.close()
+
+    return search_results
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        keyword = request.form['keyword']
+        drive = request.form['drive']
+        folder = request.form['folder']
+        print(drive, folder, keyword)
+        build_index(folder)
+        results = search_index(keyword)
+        for result in results:
+            print(result)
+            print()
+        return (results, 200);
+    else:
+        drives = [{'drive': drive} for drive in get_drives()]
+        return render_template('index.html', drives=drives)
+
+
+def get_drives():
+    drives = []
+    for drive in range(ord('A'), ord('Z') + 1):
+        drive_letter = chr(drive)
+        drive_path = drive_letter + ":\\"
+        if os.path.exists(drive_path):
+            drives.append(drive_path)
+    return drives
+
+@app.route('/get_folders')
+def get_folders():
+    path = request.args.get('path')
+    folders = [f.name for f in os.scandir(path) if f.is_dir()]
+    return jsonify(folders)
+
+@app.route('/search', methods=['POST'])
+def search():
+    keyword = request.form['keyword']
+    folder = request.form['folder']
+    create_database()
+    build_index(folder)
+    results = search_index(keyword)
+    return jsonify(results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,196 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var drives = document.getElementsByClassName("drive");
+  var foldersContainer = document.getElementById("folders-list");
+
+  // 盘符点击事件处理程序
+  for (var i = 0; i < drives.length; i++) {
+      drives[i].addEventListener("click", function() {
+          // 移除其他盘符的选中状态
+          for (var j = 0; j < drives.length; j++) {
+              drives[j].classList.remove("selected");
+          }
+
+          // 添加当前盘符的选中状态
+          this.classList.add("selected");
+
+          // 获取盘符路径
+          var drivePath = this.getAttribute("data-path");
+
+          // 加载子文件夹列表
+          loadFolders(drivePath);
+      });
+  }
+
+  // 文件夹点击事件处理程序（事件委托）
+foldersContainer.addEventListener("click", function(e) {
+  if (e.target && e.target.classList.contains("folder")) {
+      // 移除其他文件夹的选中状态
+      var folders = document.getElementsByClassName("folder");
+      for (var i = 0; i < folders.length; i++) {
+          folders[i].classList.remove("selected");
+      }
+
+      // 添加当前文件夹的选中状态
+      e.target.classList.add("selected");
+
+      // 获取文件夹路径
+      var folderPath = e.target.getAttribute("data-path");
+
+      // 更新隐藏字段的值
+      document.getElementById("folder").value = folderPath;
+
+      // 加载子文件夹列表
+      loadFolders(folderPath);
+  }
+});
+
+  // 搜索表单提交事件处理程序
+document.getElementById("search-form").addEventListener("submit", function(e) {
+  e.preventDefault(); // 阻止表单的默认提交行为
+
+  // 更新隐藏字段的值
+  var drive = document.querySelector(".drive.selected");
+  var selectedDrive = drive ? drive.getAttribute("data-path") : "";
+  document.getElementById("drive").value = selectedDrive;
+
+  var formData = new FormData(this); // 获取表单数据
+    fetch('/search', {
+        method: 'POST',
+        body: formData
+    })
+    .then(function(response) {
+        // console.log(response.json());
+        return response.json(); // 将响应数据解析为JSON格式
+    })
+    .then(function(data) {
+        renderResults(data); // 调用渲染结果的函数
+    })
+    .catch(function(error) {
+        console.log('Error:', error);
+    });
+});
+
+function renderResults(data) {
+    var container = document.getElementById('results-container');
+    container.innerHTML = ''; // 清空容器中的内容
+    console.log("start rendering results");
+    for (var filePath in data) {
+        console.log(filePath);
+        let fileData = data[filePath];
+        
+        console.log(fileData);
+        var heading = document.createElement('h3');
+        heading.textContent = fileData.file_path;
+        container.appendChild(heading);
+
+        var separator = document.createElement('hr');
+        container.appendChild(separator);
+
+        for (var lineNumber in fileData.lines) {
+            var lineContent = fileData.lines[lineNumber].line_content;
+            var line = document.createElement('p');
+            
+            var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+
+            var ln = parseInt(lineNumber) + 1;
+
+            checkbox.value = fileData.file_path + ':' + ln; // 设置复选框的值为 文件路径:行号
+            line.id = fileData.file_path + ':' + ln; // 设置行号的 id 层次;
+            line.textContent = '行号 ' + ln + ': ' + lineContent;
+            keyword = document.getElementById("keyword").value;
+            console.log(keyword);
+            line.innerHTML = highlightKeyword(line.innerHTML, keyword); // 调用高亮关键字的函数
+            // 添加到同一容器中
+            var lineContainer = document.createElement('div');
+            lineContainer.appendChild(checkbox);
+            lineContainer.appendChild(line);
+
+            container.appendChild(lineContainer);
+        }
+    }
+}
+function highlightKeyword(text, keyword) {
+  var regex = new RegExp(keyword, 'gi');
+  return text.replace(regex, '<span class="highlighted">$&</span>');
+}
+document.getElementById('save-button').addEventListener('click', function() {
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  var selectedResults = [];
+
+  checkboxes.forEach(function(checkbox) {
+      selectedResults.push(checkbox.value);
+  });
+
+  if (selectedResults.length > 0) {
+      downloadResults(selectedResults);
+  }
+});
+
+function downloadResults(selectedResults) {
+  // 创建一个包含选中结果的数据对象
+  var downloadData = {};
+  console.log(selectedResults);
+  selectedResults.forEach(function(result) {
+      var [filePath, lineNumber] = result.split(':');
+      if (!downloadData[filePath]) {
+          downloadData[filePath] = {};
+      }
+      downloadData[filePath][lineNumber] = document.getElementById(result).textContent;
+      console.log(downloadData[filePath][lineNumber]);
+  });
+
+  // 将数据转换为JSON格式
+  var jsonData = JSON.stringify(downloadData);
+
+  // 创建一个临时的Blob对象
+  var blob = new Blob([jsonData], { type: 'application/json' });
+
+  // 创建一个临时的URL对象
+  var url = URL.createObjectURL(blob);
+
+  // 创建一个隐藏的链接并触发点击事件来下载文件
+  var link = document.createElement('a');
+  link.href = url;
+  link.download = 'results.json';
+  link.style.display = 'none';
+  document.body.appendChild(link);
+  link.click();
+
+}
+
+
+  // 加载子文件夹列表
+  function loadFolders(folderPath) {
+      // 发送AJAX请求获取子文件夹列表
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+              // 清空文件夹列表容器
+              foldersContainer.innerHTML = "";
+
+              // 解析响应数据
+              var folders = JSON.parse(xhr.responseText);
+
+              // 构建子文件夹列表
+              if (folderPath !== "") {
+                  var parentFolder = document.createElement("div");
+                  parentFolder.classList.add("folder");
+                  parentFolder.setAttribute("data-path", folderPath.substring(0, folderPath.lastIndexOf("\\")));
+                  parentFolder.innerHTML = "..";
+                  foldersContainer.appendChild(parentFolder);
+              }
+
+              for (var i = 0; i < folders.length; i++) {
+                  var folder = document.createElement("div");
+                  folder.classList.add("folder");
+                  folder.setAttribute("data-path", folderPath + "\\" + folders[i]);
+                  folder.innerHTML = folders[i];
+                  foldersContainer.appendChild(folder);
+              }
+          }
+      };
+      xhr.open("GET", "/get_folders?path=" + encodeURIComponent(folderPath), true);
+      xhr.send();
+  }
+});
